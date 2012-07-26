7b7cbf22ac6d1e815fa93ecb7aade5f007a7021e
Lance Max <lancemax@lancemax01.(none)>
2012-07-25 23:42:31 -0300

    ajuste de upload de imagem

--- app/models/hospital.rb
+++ app/models/hospital.rb
@@ -3,6 +3,12 @@ class Hospital < ActiveRecord::Base
 	belongs_to :city
 	belongs_to :state
 
-  attr_accessible :address, :city_id, :complement, :description, :name, :neighborhood, :photo, :state_id, :zip_code
-  validates_presence_of :name,:address, :neighborhood, :zip_code, :city_id, :state_id
+	attr_accessible :address, :city_id, :complement, :description, :name, :neighborhood, :state_id, :zip_code, :image
+	validates_presence_of :name,:address, :neighborhood, :zip_code, :city_id, :state_id 
+
+	has_attached_file :image, 
+	:styles => { :small=> ["250"], :medium =>["400"]},
+	:path => ":rails_root/public/images/uploads/:class/:id/:style_:basename.:extension",
+	:url => "/images/uploads/:class/:id/:style_:basename.:extension",
+	:default_url => "/images/default-hospital.jpg"
 end
--- db/migrate/20120722170839_create_hospitals.rb
+++ db/migrate/20120722170839_create_hospitals.rb
@@ -9,7 +9,12 @@ class CreateHospitals < ActiveRecord::Migration
       t.integer :city_id
       t.integer :state_id
       t.text :description
-      t.string :photo
+      
+      #for image
+      t.string :image_file_name
+      t.string :image_content_type
+      t.integer :image_file_size
+      t.datetime :image_updated_at
 
       t.timestamps
     end
