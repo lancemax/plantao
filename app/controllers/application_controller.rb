ce7ca4c920b84ccd9762a2e10e35d1c7dc5f77ad
root <root@li529-127.members.linode.com>
2012-09-15 19:59:27 +0000

    meege

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,13 @@
 # -*- coding: utf-8 -*-
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  
+  before_filter :check_url
+  
+
+  def check_url
+    redirect_to request.protocol + "www." + request.host_with_port + request.fullpath if !/^www/.match(request.host) if Rails.env == 'production'
+  end
 
 
   rescue_from CanCan::AccessDenied do |exception|
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,11 @@
 class HomeController < ApplicationController
 
   def index
-  	
+  	if !current_user.nil?
+  	@user = User.find_all_by_id(current_user.id)
+    
+    p @user.name
+    end	
   end
 
 end
