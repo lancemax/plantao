55e0926c7639c977cf845876d75b370f15f1e49e
samuelteixeiras <samuelteixeiras@gmail.com>
2012-09-15 16:50:40 -0300

    ajuste envio notificacao de cadastro

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -23,16 +23,12 @@ class UserMailer < ActionMailer::Base
 
 	
 	def send_email_ownner_job(job_id,user_id)
-		p "user id"+user_id
-    	@job = Job.find_all_by_id(job_id)
+		@job = Job.find_all_by_id(job_id)
     	@user = User.find_all_by_id(user_id)
-    	p @user
-    	p 'enviar email'
-   	  	@url = "www.plantaonet.com" 
-    	mail(:to => user.email,:subject => "[PLANTÃO] "+job.area.name+" - "+job.hospital.name)
+    	@url = "www.plantaonet.com" 
+    	mail(:to => @user[0].email,:subject => "[PLANTÃO] "+@job[0].area.name+" - "+@job[0].hospital.name)
        	sleep 1
- 	
-    end
+ 	end
 
 	
 end
