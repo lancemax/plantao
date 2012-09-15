88da4a98e8b3925febffc1e3c13dba5dcdda77ee
samuelteixeiras <samuelteixeiras@gmail.com>
2012-09-15 19:58:49 -0300

    envio notificacao de fechamento de plantão 2

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -59,14 +59,14 @@ class UserMailer < ActionMailer::Base
  		@user = user
  		@job  = job
  		@url  = "www.plantaonet.com" 
-    	mail(:to => job.user.email,:subject => "[VOCÊ FOI ELEITO NO PLANTÃO] "+job.area.name+" - "+job.hospital.name)
+    	mail(:to => user.email,:subject => "[VOCÊ FOI ELEITO NO PLANTÃO] "+job.area.name+" - "+job.hospital.name)
     end
 
     def send_email_deny_job(user,job)
  		@user = user
  		@job  = job
  		@url  = "www.plantaonet.com" 
-    	mail(:to => job.user.email,:subject => "[RESULTADO DO PLANTÃO] "+job.area.name+" - "+job.hospital.name)
+    	mail(:to => user.email,:subject => "[RESULTADO DO PLANTÃO] "+job.area.name+" - "+job.hospital.name)
     end
 
 
