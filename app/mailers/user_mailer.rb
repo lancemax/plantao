3f112022b8ca9da4e4a7361b1d00c90146f85e1d
samuelteixeiras <samuelteixeiras@gmail.com>
2012-09-18 19:08:26 -0300

    comitando alterações

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -26,10 +26,10 @@ class UserMailer < ActionMailer::Base
 		@job = Job.find_all_by_id(job_id)
 		@user = User.find_all_by_id(user_id)
 	
-		UserMailer.send_email_owen_job_deliver(@user[0],@job[0]).deliver  
+		UserMailer.send_email_ownner_job_deliver(@user[0],@job[0]).deliver  
  	end
 
- 	def send_email_owen_job_deliver(user,job)
+ 	def send_email_ownner_job_deliver(user,job)
  		@user = user
  		@job  = job
  		p @job
